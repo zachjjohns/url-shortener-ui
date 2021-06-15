74fba1795e3eb7af9bdc60e586b25783d3e65f12
Zach Johnson <zachjjohns@gmail.com>
2021-06-15 11:30:51 -0500

    Refactor shortened URL render test and remove redundant cy.'s

--- cypress/integration/userFlowSpec.js
+++ cypress/integration/userFlowSpec.js
@@ -25,21 +25,23 @@ describe("URL Shortener", () => {
 
   it('Should render shortened URLs based on fetched data', () => {
     cy.get('h3').first().should('have.text', 'Awesome photo')
-    cy.get('a').first().should('have.text', 'http://localhost:3001/useshorturl/1')
-    cy.get('h3').last().should('have.text', 'Another cool photo')
-    cy.get('a').last().should('have.text', 'http://localhost:3001/useshorturl/2')
+      .get('a').first().should('have.text', 'http://localhost:3001/useshorturl/1')
+      .get('p').first().should('have.text', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+      .get('h3').last().should('have.text', 'Another cool photo')
+      .get('a').last().should('have.text', 'http://localhost:3001/useshorturl/2')
+      .get('p').last().should('have.text', 'https://i.pinimg.com/originals/f6/f7/c9/f6f7c9701f04b50baacf6a0cbf63f05c.jpg')
   })
 
   it('Should render a Form with inputs and a button', () => {
     cy.get('input[name="title"]').invoke('attr', 'placeholder').should('contain', 'Title...')
-    cy.get('input[name="urlToShorten"]').invoke('attr', 'placeholder').should('contain', 'URL to Shorten...')
-    cy.get('button').should('have.text', 'Shorten Please!')
+      .get('input[name="urlToShorten"]').invoke('attr', 'placeholder').should('contain', 'URL to Shorten...')
+      .get('button').should('have.text', 'Shorten Please!')
   })
 
   it('Should reflect what the user types within the input fields', () => {
     cy.get('input[name="title"]').type('Sweet pic')
       .should('have.value', 'Sweet pic')
-    cy.get('input[name="urlToShorten"]').type('https://c.ndtvimg.com/2019-10/o52ta3a8_sweets-_625x300_26_October_19.jpg')
+      .get('input[name="urlToShorten"]').type('https://c.ndtvimg.com/2019-10/o52ta3a8_sweets-_625x300_26_October_19.jpg')
       .should('have.value', 'https://c.ndtvimg.com/2019-10/o52ta3a8_sweets-_625x300_26_October_19.jpg')
   })
 
