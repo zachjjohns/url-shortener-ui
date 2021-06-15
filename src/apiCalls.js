e99411be4fd2b4875789ba5eac38c6a136c81bcd
Zach Johnson <zachjjohns@gmail.com>
2021-06-15 10:23:44 -0500

    Add postUrl function within apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (newUrl) => {
+  return fetch("http://localhost:3001/api/v1/urls", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: newUrl.id,
+      long_url: newUrl.long_url,
+      short_url: newUrl.short_url,
+      title: newUrl.title
+    })
+  })
+  .then(response => response.json())
+}
